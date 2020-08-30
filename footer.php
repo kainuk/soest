addeca73c3cf7720061d17a8f3c59b04763d567f
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2020-08-30 10:52:49 +0200

    Changed block scheme, and first draft fonts

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php
 	/*-----------------------------------------------------------------------------------*/
-	/* This template will be called by all other template files to finish 
+	/* This template will be called by all other template files to finish
 	/* rendering the page and display the footer area/content
 	/*-----------------------------------------------------------------------------------*/
 ?>
@@ -9,18 +9,18 @@
 
 <footer class="site-footer">
 	<div class="site-info container">
-		
-		<p>Birthed <a href="http://bckmn.com/naked-wordpress" rel="theme">Naked</a> 
-			on <a href="http://wordpress.org" rel="generator">Wordpress</a> 
-			by <a href="http://bckmn.com" rel="designer">Joshua Beckman</a>
+
+		<p>Theme  <a href="https://github.com/kainuk/soest" rel="theme">Soest</a>
+			for <a href="https://wordpress.org" rel="generator">Wordpress</a>
+			by <a href="https://eikelboom.com" rel="designer">Klaas Eikelboom</a>
 		</p>
-		
+
 	</div><!-- .site-info -->
 </footer><!-- #colophon .site-footer -->
 
-<?php wp_footer(); 
-// This fxn allows plugins to insert themselves/scripts/css/files (right here) into the footer of your website. 
-// Removing this fxn call will disable all kinds of plugins. 
+<?php wp_footer();
+// This fxn allows plugins to insert themselves/scripts/css/files (right here) into the footer of your website.
+// Removing this fxn call will disable all kinds of plugins.
 // Move it if you like, but keep it around.
 ?>
 
--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 	/*-----------------------------------------------------------------------------------*/
-	/* This template will be called by all other template files to begin 
+	/* This template will be called by all other template files to begin
 	/* rendering the page and display the header/nav
 	/*-----------------------------------------------------------------------------------*/
 ?>
@@ -10,13 +10,13 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <title>
-	<?php bloginfo('name'); // show the blog name, from settings ?> | 
+	<?php bloginfo('name'); // show the blog name, from settings ?> |
 	<?php is_front_page() ? bloginfo('description') : wp_title(''); // if we're on the home page, show the description, from the site's settings - otherwise, show the title of the post or page ?>
 </title>
 
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<?php // We are loading our theme directory style.css by queuing scripts in our functions.php file, 
+<?php // We are loading our theme directory style.css by queuing scripts in our functions.php file,
 	// so if you want to load other stylesheets,
 	// I would load them with an @import call in your style.css
 ?>
@@ -26,17 +26,17 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 
-<?php wp_head(); 
+<?php wp_head();
 // This fxn allows plugins, and Wordpress itself, to insert themselves/scripts/css/files
-// (right here) into the head of your website. 
-// Removing this fxn call will disable all kinds of plugins and Wordpress default insertions. 
+// (right here) into the head of your website.
+// Removing this fxn call will disable all kinds of plugins and Wordpress default insertions.
 // Move it if you like, but I would keep it around.
 ?>
 
 </head>
 
-<body 
-	<?php body_class(); 
+<body
+	<?php body_class();
 	// This will display a class specific to whatever is being loaded by Wordpress
 	// i.e. on a home page, it will return [class="home"]
 	// on a single post, it will return [class="single postid-{ID}"]
@@ -45,26 +45,22 @@
 >
 
 <header id="masthead" class="site-header">
-	<div class="container center">
-	
-		<nav class="site-navigation main-navigation">
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); // Display the user-defined menu in Appearance > Menus ?>
-		</nav><!-- .site-navigation .main-navigation -->
-	</div>
-	<div class="center">
-
-		<div id="brand">
-			<h1 class="site-title">
-				<a href="<?php echo esc_url( home_url( '/' ) ); // Link to the home page ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); // Title it with the blog name ?>" rel="home"><?php bloginfo( 'name' ); // Display the blog name ?></a>
-			</h1>
-			<h4 class="site-description">
-				<?php bloginfo( 'description' ); // Display the blog description, found in General Settings ?>
-			</h4>
-		</div><!-- /brand -->
-		
-		<div class="clear"></div>
-	</div><!--/container -->
-		
+    <div class="container center">
+        <h1>
+          <a href="<?php echo esc_url( home_url( '/' ) ); // Link to the home page ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); // Title it with the blog name ?>" rel="home"><?php bloginfo( 'name' ); // Display the blog name ?></a>
+        </h1>
+        <h6>
+          <?php bloginfo( 'description' ); // Display the blog description, found in General Settings ?>
+        </h6>
+    </div><!--/container -->
+	<div class="container top-menu">
+        <nav class="site-navigation">
+          <?php wp_nav_menu(
+            [
+              'theme_location' => 'primary',
+            ]); // Display the user-defined menu in Appearance > Menus ?>
+        </nav><!-- .site-navigation .main-navigation -->
+    </div>
 </header><!-- #masthead .site-header -->
 
-<main class="main-fluid"><!-- start the page containter -->
+<main class="container"><!-- start the page container -->
